902aea6c6d2f32d7bef68349c75ecb73ceeb2743
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-05-27 22:40:47 +0800

    Learned how to use properly remainder operator

--- script.js
+++ script.js
@@ -325,30 +325,52 @@ btnSort.addEventListener('click', function (e) {
 // console.log(Number.isFinite('20'));
 // console.log(Number.isFinite(23 / 0));
 
-console.log(Math.sqrt(25));
-console.log(Math.max(5, 18, '23', 11, 2));
+// console.log(Math.sqrt(25));
+// console.log(Math.max(5, 18, '23', 11, 2));
 
-console.log(Math.PI * Number.parseFloat('10px') ** 2);
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
 
-console.log(Math.floor(Math.random() * 6) + 1);
+// console.log(Math.floor(Math.random() * 6) + 1);
 
-// Rounding Integers
-console.log(Math.trunc(23.3));
+// // Rounding Integers
+// console.log(Math.trunc(23.3));
 
-console.log(Math.round(23.3));
-console.log(Math.round(23.9));
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
 
-console.log(Math.ceil(23.3));
-console.log(Math.round(23.9));
+// console.log(Math.ceil(23.3));
+// console.log(Math.round(23.9));
 
-console.log(Math.floor(23.3));
-console.log(Math.floor(23.9));
+// console.log(Math.floor(23.3));
+// console.log(Math.floor(23.9));
 
-console.log(Math.trunc(-23.3));
-console.log(Math.floor(-23.3));
+// console.log(Math.trunc(-23.3));
+// console.log(Math.floor(-23.3));
 
-//Rounding decimals
-console.log((2.7).toFixed(0));
-console.log((2.7).toFixed(3));
-console.log((2.345).toFixed(2));
-console.log(+(2.345).toFixed(2));
+// //Rounding decimals
+// console.log((2.7).toFixed(0));
+// console.log((2.7).toFixed(3));
+// console.log((2.345).toFixed(2));
+// console.log(+(2.345).toFixed(2));
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1, so 1 is the remainder
+console.log(8 % 3);
+
+console.log(6 % 2); // 0
+console.log(6 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(
+    function (row, i) {
+      if (i % 2 === 0)
+        row.style.backgroundColor = 'orangered';
+      if (i % 3 === 0) row.style.backgroundColor = 'blue';
+    }
+  );
+});
